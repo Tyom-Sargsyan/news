64ec55b143d64d51a779ea41afaefffc83e2f6c2
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 <hnairiareg@gmail.com>
2023-03-20 06:59:30 +0400

    Moved front logic to back

--- src/pages/api/newsAm.js
+++ src/pages/api/newsAm.js
@@ -1,11 +1,66 @@
 const axios = require("axios");
+const HTMLParser = require("node-html-parser");
+
+const newsAmLanguages = { en: "eng", hy: "arm", ru: "rus" };
+
+const newsAmDomainNormalizer = (link) =>
+  link.includes("news.am") ? link : `https://news.am/${link}`;
+
+const dateFormatter = new Intl.DateTimeFormat("en-us", {
+  weekday: "short",
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+const dateNormalizer = (date) => {
+  const moddedDate = date
+    .replaceAll(",", "")
+    .replaceAll(":", ".")
+    .replaceAll(" ", ".")
+    .split(".");
+
+  return dateFormatter.format(
+    new Date(
+      moddedDate[4],
+      moddedDate[3] - 1,
+      moddedDate[2],
+      moddedDate[0],
+      moddedDate[1]
+    )
+  );
+};
+
+const zeroAdder = (number) => (number < 10 ? "0" + number : number);
 
 export default function handler(req, res) {
   axios
     .get(
-      `https://news.am/${req.query.lang}/news/allregions/allthemes/${req.query.year}/${req.query.month}/${req.query.day}/`
+      `https://news.am/${
+        newsAmLanguages[req.query.lang]
+      }/news/allregions/allthemes/${req.query.year}/${zeroAdder(
+        req.query.month
+      )}/${zeroAdder(req.query.day)}/`
     )
-    .then(({ data }) => {
-      res.status(200).send(data);
+    .then(({ data }) => HTMLParser.parse(data))
+    .then((parsedData) => {
+      const news = [];
+      parsedData.querySelectorAll("article.article-item").forEach((article) =>
+        news.push({
+          link: newsAmDomainNormalizer(
+            article.querySelector("a").getAttribute("href")
+          ),
+          img: newsAmDomainNormalizer(
+            article.querySelector("a img").getAttribute("src")
+          ),
+          title: article.querySelector(".title a").innerText,
+          date: dateNormalizer(article.querySelector("time").innerText),
+          description: article.querySelector(".text").innerText,
+        })
+      );
+
+      res.status(200).send(news);
     });
 }
--- src/services/newsAm.js
+++ src/services/newsAm.js
@@ -1,63 +1,7 @@
-import { parse } from "node-html-parser";
-
-const newsAmLanguages = { en: "eng", hy: "arm", ru: "rus" };
-
-const domainNormalizer = (link) =>
-  link.includes("news.am") ? link : `https://news.am/${link}`;
-
-const dateFormatter = new Intl.DateTimeFormat("en-us", {
-  weekday: "short",
-  year: "numeric",
-  month: "short",
-  day: "2-digit",
-  hour: "2-digit",
-  minute: "2-digit",
-});
-
-const dateNormalizer = (date) => {
-  const moddedDate = date
-    .replaceAll(",", "")
-    .replaceAll(":", ".")
-    .replaceAll(" ", ".")
-    .split(".");
-
-  return dateFormatter.format(
-    new Date(
-      moddedDate[4],
-      moddedDate[3] - 1,
-      moddedDate[2],
-      moddedDate[0],
-      moddedDate[1]
-    )
-  );
-};
-
-const zeroAdder = (number) => (number < 10 ? "0" + number : number);
-
-export const getNewsByDate = async ({ year, month, day, language }) =>
+const getNewsByDate = async ({ year, month, day, language }) =>
   await fetch(
-    `api//newsAm?year=${year}&month=${zeroAdder(month)}&day=${zeroAdder(
-      day
-    )}&lang=${newsAmLanguages[language]}`
-  )
-    .then((response) => response.text())
-    .then((data) => parse(data))
-    .then((dom) => {
-      const news = [];
-      dom.querySelectorAll("article.article-item").forEach((article) =>
-        news.push({
-          link: domainNormalizer(
-            article.querySelector("a").getAttribute("href")
-          ),
-          img: domainNormalizer(
-            article.querySelector("a img").getAttribute("src")
-          ),
-          title: article.querySelector(".title a").innerText,
-          date: dateNormalizer(article.querySelector("time").innerText),
-          description: article.querySelector(".text").innerText,
-        })
-      );
-      return news;
-    });
+    `api//newsAm?year=${year}&month=${month}&day=${day}&lang=${language}`
+  ).then((response) => response.json());
+
 
 export default getNewsByDate;
